cb1f54dc4455cd5ed94162a753f99cdd956a6d66
j3s30p <57714047+j3s30p@users.noreply.github.com>
2026-07-13 10:28:43 +0900

    AiUsage 1.0.1 배포

--- Casks/aiusage.rb
+++ Casks/aiusage.rb
@@ -1,6 +1,6 @@
 cask "aiusage" do
-  version "1.0.0"
-  sha256 "8639710f47e673008977bbd3ed0344677ca76333461864e3670f76856adb64bf"
+  version "1.0.1"
+  sha256 "f97fb46e2db205aaf1943be16364bdc7da2637b360d73be5ff006cd7b0146ee6"
 
   url "https://github.com/j3s30p/AI_Usage/releases/download/v#{version}/AiUsage-v#{version}-macos-universal.zip"
   name "AiUsage"
